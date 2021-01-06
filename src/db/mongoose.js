d64090a085786a7d25bf40863246da1ac3195d18
Tsuneki Umeda <umedat@amazon.co.jp>
2021-01-06 17:15:55 +0900

    Structuring REST API

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,63 +1,9 @@
 const mongoose = require('mongoose')
-const validator = require('validator')
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
   useNewUrlParser: true,
   useCreateIndex: true,
 })
-
-const User = mongoose.model('User', {
-  name: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    trim: true,
-    lowercase: true,
-    validate(value) {
-      if (!validator.isEmail(value)) {
-        throw new Error('Email us invalid')
-      }
-    },
-  },
-  age: {
-    type: Number,
-    default: 0,
-    validate(value) {
-      if (value < 0) {
-        throw new Error('Age must be a positive number')
-      }
-    },
-  },
-  password: {
-    type: String,
-    trim: true,
-    required: true,
-    minlength: 7,
-    validate(value) {
-      if (value.toLowerCase().includes('password')) {
-        throw new Error('cannot contain password')
-      }
-    },
-  },
-})
-
-const me = new User({
-  name: ' Mike  ',
-  email: 'myemail@example.com',
-  password: 'pass1234',
-})
-
-me.save()
-  .then(() => {
-    console.log(me)
-  })
-  .catch(error => {
-    console.log('Error', error)
-  })
 
 const Task = mongoose.model('Task', {
   description: {
@@ -70,17 +16,3 @@ const Task = mongoose.model('Task', {
     default: false,
   },
 })
-
-const task = new Task({
-  description: 'brain dump',
-  completed: false,
-})
-
-task
-  .save()
-  .then(() => {
-    console.log(task)
-  })
-  .catch(error => {
-    console.log(error)
-  })
